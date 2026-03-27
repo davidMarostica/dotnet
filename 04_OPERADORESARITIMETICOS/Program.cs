b501061893f76ca6a478c3bd20eff80ed455e13e
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-27 14:52:46 -0300

    Operadore Aritimetricos

---
+++ 04_OPERADORESARITIMETICOS/Program.cs
@@ -0,0 +1,14 @@
+﻿using _04_OPERADORESARITIMETICOS.Models;
+
+// string a = "15";
+
+// int b = Convert.ToInt32(a);
+
+// Console.WriteLine(b);
+
+string a = "15-";
+
+int.TryParse(a, out int b);
+
+Console.WriteLine(b);
+Console.WriteLine("Fim do programa");
