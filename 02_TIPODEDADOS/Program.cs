119a287672d2da413640cc276e83c512f2afaa04
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-27 12:45:13 -0300

    Tipos De Dados

---
+++ 02_TIPODEDADOS/Program.cs
@@ -0,0 +1,24 @@
+﻿using  _02_TIPODEDADOS.Models;
+
+string apresentacao = "Olá, seja bem vindo ao curso de C#";
+
+int quantidade = 1;
+quantidade = 10;
+
+double altura = 1.80;
+
+bool condicao = true;
+
+decimal valor = 1.80m;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variavel: " + quantidade);
+Console.WriteLine("Valor da variavel: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variavel: " + altura);
+Console.WriteLine("Valor da variavel: " + condicao);
+Console.WriteLine("Valor da variavel: " + valor);
+
+// Tipo data DateTime
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine("Data atual: " + dataAtual);  
