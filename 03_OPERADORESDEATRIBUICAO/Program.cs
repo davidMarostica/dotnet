c7c0ea7d741a6f811f60aeb1acc0d0177c1aeadf
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-27 14:33:50 -0300

    Operadores de atruição

---
+++ 03_OPERADORESDEATRIBUICAO/Program.cs
@@ -0,0 +1,34 @@
+﻿using _03_OPERADORESDEATRIBUICAO.Models;
+
+{
+    // cast - castng - conversão de tipos
+
+   // int a = Convert.ToInt32("5");
+  
+
+// int a = 10;
+// a += 5; // operador de atribuição composto, equivalente a a = a + 5
+// Console.WriteLine("Valor de a: " + a);
+// int b = 20; 
+
+// decimal c = a + b; // operador de atribuição
+// c +=  2; 
+// Console.WriteLine("Valor de c: " + c);
+
+
+// int inteiro = 5;
+// string a = inteiro.ToString(); // agora sim, chama o método
+
+// Console.WriteLine(a);
+
+// int a  = 5; 
+// double b = a;
+
+// int a = int.MaxValue;
+// long b = a;
+// Console.WriteLine(b);
+
+double a = 4 / (2 + 0);
+Console.WriteLine(a);
+
+}
