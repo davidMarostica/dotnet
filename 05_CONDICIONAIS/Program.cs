8e8d8fc40ef5191d145d3b3bb16ae98d43f0fe53
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-27 15:27:46 -0300

    Operadores logico

---
+++ 05_CONDICIONAIS/Program.cs
@@ -0,0 +1,14 @@
+﻿using _05_CONDICIONAIS.Models;
+{
+    int quantidadeEmEstoque = 10;
+    int quantidadeCompra = 4;
+
+    if (quantidadeCompra <= quantidadeEmEstoque)
+    {
+        Console.WriteLine("Venda realizada com sucesso");
+    }
+    else
+    {
+        Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+    }
+}
