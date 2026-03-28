71d40d2518096efbbfc50d23bef20451c16556ae
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 12:01:33 -0300

    Operadores Aritiméticos

---
+++ 08_ARITIMETICO/Models/Calculadora.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _08_ARITIMETICO.Models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+           Console.WriteLine($" {x} + {y} = {x + y}"); 
+        }
+
+            public void Subtrair(int x, int y)
+            {
+            Console.WriteLine($" {x} - {y} = {x - y}"); 
+            }
+    
+            public void Multiplicar(int x, int y)
+            {
+            Console.WriteLine($" {x} * {y} = {x * y}"); 
+            }
+    
+            public void Dividir(int x, int y)
+            {
+                if (y != 0)
+                {
+                    Console.WriteLine($" {x} / {y} = {(double)x / y}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                }
+            }
+    }
+}
---
+++ 08_ARITIMETICO/Program.cs
@@ -0,0 +1,12 @@
+﻿using _08_ARITIMETICO.Models;
+
+Calculadora calc = new Calculadora();
+
+
+calc.Somar(10, 5);
+calc.Subtrair(10, 5);   
+calc.Multiplicar(10, 5);
+calc.Dividir(10, 5);
+calc.Dividir(10, 0);    
+
+
