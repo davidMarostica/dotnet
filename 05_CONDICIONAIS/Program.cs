e0aa95fb2651f28a5d39b59202c101cf6a5cc845
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 08:16:33 -0300

    Alinhando o if

--- 05_CONDICIONAIS/Program.cs
+++ 05_CONDICIONAIS/Program.cs
@@ -1,26 +1,27 @@
 ﻿using _05_CONDICIONAIS.Models;
 {
     int quantidadeEmEstoque = 10;
-    int quantidadeCompra = 4;
-    bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+    int quantidadeCompra = 0;
+    bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
     Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
     Console.WriteLine($"Quantidade desejada: {quantidadeCompra}");  
     Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-     if (possivelVenda)
-
-    {
-        if (possivelVenda)
+     if (quantidadeCompra == 0)
+        {
+            Console.WriteLine("Venda inválida.");
+        }
+        
+    
+     else if (possivelVenda)
         {
             Console.WriteLine("Venda realizada com sucesso!");
         }
-        else
+     else
         {
             Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque.");
-
         }
-    }
+       
 
 }
-
