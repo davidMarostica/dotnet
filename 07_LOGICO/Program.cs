99307a9679548197493a2252341cb6b4fceec173
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 10:26:29 -0300

    Operador Lógico OR

---
+++ 07_LOGICO/Program.cs
@@ -0,0 +1,13 @@
+﻿using _07_LOGICO.Models;
+
+bool ehMaiorDeIdade = true;
+bool possuiAutorizacaoDoResponsavel = false;
+
+if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+{
+    Console.WriteLine("Acesso permitido.");
+}
+else
+{
+    Console.WriteLine("Acesso negado.");
+}
