c1554dca27e34931e96dc28b6fd30453384995d1
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 13:06:28 -0300

    Logica aritimetica

--- 08_ARITIMETICO/Models/Calculadora.cs
+++ 08_ARITIMETICO/Models/Calculadora.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace _08_ARITIMETICO.Models
 {
@@ -9,29 +6,65 @@ namespace _08_ARITIMETICO.Models
     {
         public void Somar(int x, int y)
         {
-           Console.WriteLine($" {x} + {y} = {x + y}"); 
+            Console.WriteLine($" {x} + {y} = {x + y}"); 
         }
 
-            public void Subtrair(int x, int y)
-            {
+        public void Subtrair(int x, int y)
+        {
             Console.WriteLine($" {x} - {y} = {x - y}"); 
-            }
-    
-            public void Multiplicar(int x, int y)
-            {
+        }
+
+        public void Multiplicar(int x, int y)
+        {
             Console.WriteLine($" {x} * {y} = {x * y}"); 
-            }
-    
-            public void Dividir(int x, int y)
+        }
+
+        public void Dividir(int x, int y)
+        {
+            if (y != 0)
+                Console.WriteLine($" {x} / {y} = {(double)x / y}");
+            else
+                Console.WriteLine("Erro: Divisão por zero não é permitida.");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($" {x} ^ {y} = {pot}");
+        }
+
+        public void RaizQuadrada(int x)
+        {
+            if (x >= 0)
             {
-                if (y != 0)
-                {
-                    Console.WriteLine($" {x} / {y} = {(double)x / y}");
-                }
-                else
-                {
-                    Console.WriteLine("Erro: Divisão por zero não é permitida.");
-                }
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine($" √{x} = {raiz}");
             }
+            else
+            {
+                Console.WriteLine("Erro: Não é possível calcular a raiz quadrada de um número negativo.");
+            }
+        }
+
+        public void Seno(double angulo)
+        {
+            double radianos = angulo * (Math.PI / 180);
+            double seno = Math.Sin(radianos);
+            Console.WriteLine($"Seno de {angulo}° = {seno}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radianos = angulo * (Math.PI / 180);
+            double coseno = Math.Cos(radianos);
+            Console.WriteLine($"Cosseno de {angulo}° = {coseno}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radianos = angulo * (Math.PI / 180);
+            double tangente = Math.Tan(radianos);
+            Console.WriteLine($"Tangente de {angulo}° = {tangente}");
+        }
     }
 }
--- 08_ARITIMETICO/Program.cs
+++ 08_ARITIMETICO/Program.cs
@@ -8,5 +8,11 @@ calc.Subtrair(10, 5);
 calc.Multiplicar(10, 5);
 calc.Dividir(10, 5);
 calc.Dividir(10, 0);    
+calc.Potencia(2, 3);
+calc.RaizQuadrada(16);
+calc.RaizQuadrada(-4);  
+calc.Seno(30);
+calc.Coseno(60);
+calc.Tangente(45);
 
 
