b93082785c2b937d1a7f757bf25337bd406ab188
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 07:41:39 -0300

    estrutura condicional

--- 05_CONDICIONAIS/Program.cs
+++ 05_CONDICIONAIS/Program.cs
@@ -1,14 +1,20 @@
 ﻿using _05_CONDICIONAIS.Models;
 {
-    int quantidadeEmEstoque = 10;
+    int quantidadeEmEstoque = 3;
     int quantidadeCompra = 4;
+    bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-    if (quantidadeCompra <= quantidadeEmEstoque)
     {
-        Console.WriteLine("Venda realizada com sucesso");
+        if (possivelVenda)
+        {
+            Console.WriteLine("Venda realizada com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque.");
+
+        }
     }
-    else
-    {
-        Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
-    }
+
 }
+
