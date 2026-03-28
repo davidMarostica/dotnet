857083771167bbf8a40bb9bf8485b4bbc48b40c9
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 10:55:52 -0300

    Operador lógico &&

--- 07_LOGICO/Program.cs
+++ 07_LOGICO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using _07_LOGICO.Models;
 
+// Operador Lógico OR (||) qualquer uma das condições ser verdadeira, o resultado é verdadeiro
 bool ehMaiorDeIdade = true;
 bool possuiAutorizacaoDoResponsavel = false;
 
@@ -10,4 +11,18 @@ if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
 else
 {
     Console.WriteLine("Acesso negado.");
+}
+
+//Operador Lógico && (AND) tem que cumprir todas as condições para ser verdadeiro
+
+bool possuiPresencaMinima = true;
+double media = 7.5;
+
+if (possuiPresencaMinima && media >= 7)
+{
+    Console.WriteLine("Aluno aprovado.");
+}
+else
+{
+    Console.WriteLine("Aluno reprovado.");
 }
