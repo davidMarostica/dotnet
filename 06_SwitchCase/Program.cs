4099039af8b1be7ae08a9b9130f763c4b1a7efa1
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-28 08:40:07 -0300

    Usando Switch Case

---
+++ 06_SwitchCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using  _06_SwitchCase.Models;
+
+{
+    Console.WriteLine("Digite uma letra");
+    string letra = Console.ReadLine().ToLower();
+
+
+    switch (letra)
+    {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+            Console.WriteLine("A letra é uma vogal.");
+            break;
+        default:
+            Console.WriteLine("A letra é uma consoante.");
+            break;
+    }
+}
