45a7c70e0498715ae2ffaca08ba042e3d1011b19
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-29 08:44:54 -0300

    Usando condição For e While

---
+++ 09_REPETICAO/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using _09_REPETICAO.Models;
+
+// for (inicialização; condição; incremento)
+Console.WriteLine("-------------Tabuada do 10-------------");
+ int numro = 10;
+  
+
+  for(int contador = 0; contador <= 10; contador++)
+  {
+    Console.WriteLine($"{numro} x {contador} = {numro * contador}");
+  }
+ 
+ Console.WriteLine("-------------Tabuada do 5-------------");
+
+  //while (condição)
+  int numero = 5;
+  int contador2 = 0;
+  while (contador2 <= 10)
+  {
+    Console.WriteLine($"{numero} x {contador2} = {numero * contador2}");
+    contador2++;
+  }
