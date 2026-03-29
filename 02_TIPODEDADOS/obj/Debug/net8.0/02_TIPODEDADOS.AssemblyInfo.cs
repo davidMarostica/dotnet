c18b63acc8f8ee7cc71cde814f265b21b6896a6b
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-03-29 07:35:42 -0300

    Estudando estrutura de repetição

--- 02_TIPODEDADOS/obj/Debug/net8.0/02_TIPODEDADOS.AssemblyInfo.cs
+++ 02_TIPODEDADOS/obj/Debug/net8.0/02_TIPODEDADOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02_TIPODEDADOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4099039af8b1be7ae08a9b9130f763c4b1a7efa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204ba4cced9635c8456784d8102180e1fc9cc6f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("02_TIPODEDADOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02_TIPODEDADOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
