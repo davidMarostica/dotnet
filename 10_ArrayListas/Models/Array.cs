92b4166c3877eb6a85c33441c820c2a3b4f7180c
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-04-03 08:01:50 -0300

    Criando array

---
+++ 10_ArrayListas/Models/Array.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _10_ArrayListas.Models
+{
+    public class Array
+    {
+        
+    }
+}
---
+++ 10_ArrayListas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Contracts;
+using _10_ArrayListas.Models;
+
+// array é uma estrutura de dados que armazena um número fixo de valores do mesmo tipo.
+ int[] arrayIinteiros = new int[5];
+    arrayIinteiros[0] = 10;
+    arrayIinteiros[1] = 20;
+    arrayIinteiros[2] = 30;
+    arrayIinteiros[3] = 40;
+    arrayIinteiros[4] = 50;
+
+
+    for (int contador = 0; contador < arrayIinteiros.Length; contador++)
+    {
+        Console.WriteLine($"Posição: N {contador} - Valor: {arrayIinteiros[contador]}");
+    }
