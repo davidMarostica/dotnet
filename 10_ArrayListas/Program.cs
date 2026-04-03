adb2f3d446d41d89d304e27c8d69bb667c1fcadf
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-04-03 09:34:11 -0300

    addiciondo listas

--- 10_ArrayListas/Program.cs
+++ 10_ArrayListas/Program.cs
@@ -33,15 +33,33 @@ Console.WriteLine("\nArray com foreach:");
     listaString.Add("São Paulo");
     listaString.Add("Rio de Janeiro");
     listaString.Add("Belo Horizonte");
-    Console.WriteLine("\nLista de cidades:");
+    listaString.Add("Curitiba");
+    listaString.Add("Salvador");
+
+
+    Console.WriteLine($"\nItens na minha lista: {listaString.Count} - Capacidade:{listaString.Capacity}");
+
+    listaString.Add("Fortaleza");
+
+    Console.WriteLine($"\nItens na minha lista: {listaString.Count} - Capacidade:{listaString.Capacity}");
+    listaString.Remove("Belo Horizonte");
+
+    Console.WriteLine($"\nItens na minha lista: {listaString.Count} - Capacidade:{listaString.Capacity}");
+     
+    //  A propriedade Count retorna o número de elementos atualmente armazenados na lista, enquanto a propriedade Capacity retorna a capacidade total da lista, ou seja, o número de elementos que a lista pode armazenar antes de precisar ser redimensionada.
     for (int contador = 0; contador < listaString.Count; contador++)
     {
         Console.WriteLine($"Posição: N {contador} - Valor: {listaString[contador]}");
     } 
 
     int contadorForeachLista = 0;
-     Console.WriteLine("\nLista de cidades com foreach:");
+     Console.WriteLine($"\nLista de cidades com foreach:");
+
+    //  O foreach é uma estrutura de repetição que percorre cada elemento de uma coleção, como um array ou uma lista, e executa um bloco de código para cada elemento. Ele é especialmente útil para iterar sobre coleções sem a necessidade de gerenciar índices manualmente.
     foreach (string cidade in listaString)
     {
         Console.WriteLine($"Valor: {cidade}");
     }
+
+    Console.WriteLine($"\nItens na minha lista: {listaString.Count} - Capacidade:{listaString.Capacity}");
+
